ba5822724c3df972f98fc4d56ded8b9a5f120bd6
Lucio Paiva <luciopaiva@gmail.com>
2018-08-31 23:11:51 -0300

    Distribution bars

--- index.js
+++ index.js
@@ -20,6 +20,42 @@ class Galton {
         this.canvasWidth = 800;
         this.canvasHeight = 600;
 
+        this.spacing = 30;
+        this.pinsHorizontalSpacing = 2 * this.spacing;
+        this.pinsVerticalSpacing = this.spacing;
+        this.bottomPinRowSize = 9;
+        this.pinPyramidHeight = (this.bottomPinRowSize - 1) * this.pinsVerticalSpacing;
+        this.topPinY = 100;
+
+        this.tubeHeight = this.pinPyramidHeight;  // just make it the same height as the pyramid
+        this.halfTubeHeight = this.tubeHeight / 2;
+        this.tubesY = this.topPinY + this.pinPyramidHeight + this.spacing + this.halfTubeHeight;
+        this.numberOfTubeWalls = this.bottomPinRowSize * 2 + 1;
+        this.tubesHorizontalSpacing = this.pinsHorizontalSpacing / 2;
+        this.tubesWidth = (this.numberOfTubeWalls - 1) * this.tubesHorizontalSpacing;
+        this.halfTubesWidth = this.tubesWidth / 2;
+
+        this.numberOfSensors = this.numberOfTubeWalls - 1;
+        this.sensorsWidth = (this.numberOfSensors - 1) * this.tubesHorizontalSpacing;
+        this.halfSensorsWidth = this.sensorsWidth / 2;
+        this.sensorsY = this.tubesY + this.halfTubeHeight;
+        this.sensorSize = .8 * this.tubesHorizontalSpacing;
+
+        this.sensorIndexBySensor = new Map();
+        this.sensorCounters = Array.from(Array(this.numberOfSensors), () => 0);
+        this.topCounter = 0;
+
+        this.distributionBarsManualBaseAdjust = -4;
+        this.distributionBarsManualDeltaAdjust = -.9;
+        this.distributionBarsX = this.canvasWidth / 2 - this.halfSensorsWidth + this.distributionBarsManualBaseAdjust;
+        this.distributionBarsY = this.tubesY - this.halfTubeHeight;
+
+        this.backgroundCanvas = document.getElementById("background-canvas");
+        this.backgroundCanvas.width = this.canvasWidth;
+        this.backgroundCanvas.height = this.canvasHeight;
+        /** @type {CanvasRenderingContext2D} */
+        this.backgroundContext = this.backgroundCanvas.getContext("2d");
+
         this.engine = Engine.create();
         this.world = this.engine.world;
         this.render = Render.create({
@@ -30,7 +66,7 @@ class Galton {
                 height: this.canvasHeight,
                 showAngleIndicator: false,
                 wireframes: false,
-                // background: "transparent",
+                background: "transparent",
             }
         });
 
@@ -39,17 +75,38 @@ class Galton {
         const runner = Runner.create();
         Runner.run(runner, this.engine);
 
-        this.bottomPinRowSize = 9;
-        this.makePins();
-        // this.makeBeads();
+        this.makePinsPyramid();
         this.makeBead();
         // this.makeFunnel();
-        this.makeSlots();
+        this.makeTubes();
+        this.redrawDistributionsBars();
 
         Render.lookAt(this.render, Composite.allBodies(this.world));
 
+        Events.on(this.engine, "collisionEnd", this.onCollisionEnd.bind(this));
+
         setInterval(this.makeBead.bind(this), 250);
         setInterval(this.removeFallenBeads.bind(this), 5000);
+    }
+
+    onCollisionEnd (event) {
+        let hadChange = false;
+
+        for (const pair of event.pairs) {
+
+            const index = this.sensorIndexBySensor.get(pair.bodyA) || this.sensorIndexBySensor.get(pair.bodyB);
+            if (index) {
+                this.sensorCounters[index]++;
+                if (this.topCounter < this.sensorCounters[index]) {
+                    this.topCounter = this.sensorCounters[index];
+                }
+                hadChange = true;
+            }
+        }
+
+        if (hadChange) {
+            this.redrawDistributionsBars();
+        }
     }
 
     /**
@@ -77,12 +134,6 @@ class Galton {
         }));
     }
 
-    makeBeads() {
-        const stack = Composites.stack(50, 0, 10, 10, 0, 0,
-            (x, y) => Bodies.circle(x, y, 8, { friction: 1e-5, restitution: 0.0, density: 1e-3 }));
-        World.add(this.world, stack);
-    }
-
     makeFunnel() {
         World.add(this.world, [
             Bodies.rectangle(55, 150, 200, 10, { isStatic: true, angle: rad(45) }),
@@ -93,18 +144,15 @@ class Galton {
         ]);
     }
 
-    makePins() {
-        const baseX = 0;
-        const baseY = 100;
-        const hspacing = 60;
-        const vspacing = 30;
+    makePinsPyramid() {
         for (let y = 0; y < this.bottomPinRowSize; y++) {
-            const width = y * hspacing;
-            const shiftToCentralize = width / 2;
+            const rowWidth = y * this.pinsHorizontalSpacing;
+            const shiftToCentralize = rowWidth / 2;
+
             for (let x = 0; x <= y; x++) {
                 World.add(this.world, Bodies.circle(
-                    baseX - shiftToCentralize + x * hspacing,
-                    baseY + y * vspacing,
+                    x * this.pinsHorizontalSpacing - shiftToCentralize,
+                    this.topPinY + y * this.pinsVerticalSpacing,
                     5, {
                         isStatic: true,
                         render: { fillStyle: "#d138d3" },
@@ -114,92 +162,49 @@ class Galton {
         }
     }
 
-    makeSlots() {
-        const pinsHeight = (this.bottomPinRowSize - 1) * 30;
-        const wallHeight = pinsHeight;
-        const baseX = 0;
-        const baseY = 100 + pinsHeight + wallHeight / 2 + 30;
-        const hspacing = 30;  // must be half of pins hspacing
-        const numberOfWalls = this.bottomPinRowSize * 2 + 1;
-        const width = (numberOfWalls - 1) * hspacing;
-        const shiftLeft = width / 2;
-
-        // walls
-        for (let x = 0; x < numberOfWalls; x++) {
+    makeTubes() {
+        // tubes walls
+        for (let x = 0; x < this.numberOfTubeWalls; x++) {
             World.add(this.world, Bodies.rectangle(
-                baseX - shiftLeft + x * hspacing,
-                baseY, wallHeight, 1, {
+                x * this.tubesHorizontalSpacing - this.halfTubesWidth,
+                this.tubesY, this.tubeHeight, 1, {
                     isStatic: true,
                     angle: rad(90),
-                    render: { fillStyle: "#f0f0f0" },
+                    render: { fillStyle: "#2b7b1b" },
                 })
             );
         }
 
-        let numberOfSensors = numberOfWalls - 1;
-        const sensorsWidth = (numberOfSensors - 1) * hspacing;
-        const sensorShiftLeft = sensorsWidth / 2;
-        const sensorBaseY = baseY + wallHeight / 2;
-        const sensorSize = .8 * hspacing;
-
         // sensors
-        for (let x = 0; x < numberOfSensors; x++) {
-            World.add(this.world, Bodies.rectangle(
-                baseX - sensorShiftLeft + x * hspacing,
-                sensorBaseY - sensorSize, sensorSize, sensorSize, {
+        for (let i = 0; i < this.numberOfSensors; i++) {
+            const sensor = Bodies.rectangle(
+                i * this.tubesHorizontalSpacing - this.halfSensorsWidth,
+                this.sensorsY - this.sensorSize,
+                this.sensorSize, this.sensorSize, {
                     isSensor: true,
                     isStatic: true,
-                    render: { fillStyle: "#590000" },
-                })
-            );
+                    render: { fillStyle: "transparent" },
+                });
+
+            this.sensorIndexBySensor.set(sensor, i);
+            World.add(this.world, sensor);
+        }
+    }
+
+    redrawDistributionsBars() {
+        this.backgroundContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        this.backgroundContext.fillStyle = "#000000";
+        this.backgroundContext.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+        for (let i = 0; i < this.numberOfSensors; i++) {
+            const x = this.distributionBarsX + i * (this.spacing + this.distributionBarsManualDeltaAdjust);
+            const height = this.tubeHeight * this.sensorCounters[i] / this.topCounter;
+            const y = this.tubeHeight - height;
+
+            this.backgroundContext.fillStyle = "#2a780f";
+            this.backgroundContext.fillRect(x, this.distributionBarsY + y, this.sensorSize, height);
         }
     }
 }
 
 new Galton();
-
-/*
-
-// add bodies
-const stack = Composites.stack(20, 20, 20, 20, 0, 0,
-    (x, y) => Bodies.circle(x, y, 5, { friction: 0.00001, restitution: 0.5, density: 0.001 }));
-
-World.add(world, stack);
-
-
-
-// World.add(world, [
-//     Bodies.rectangle(200, 150, 700, 20, { isStatic: true, angle: Math.PI * 0.06 }),
-//     Bodies.rectangle(500, 350, 700, 20, { isStatic: true, angle: -Math.PI * 0.06 }),
-//     Bodies.rectangle(340, 580, 700, 20, { isStatic: true, angle: Math.PI * 0.04 })
-// ]);
-
-// add mouse control
-const mouse = Mouse.create(render.canvas),
-    mouseConstraint = MouseConstraint.create(engine, {
-        mouse: mouse,
-        constraint: {
-            stiffness: 0.2,
-            render: {
-                visible: false
-            }
-        }
-    });
-
-World.add(world, mouseConstraint);
-
-// keep the mouse in sync with rendering
-render.mouse = mouse;
-
-// fit the render viewport to the scene
-Render.lookAt(render, Composite.allBodies(world));
-
-// wrapping using matter-wrap plugin
-for (let i = 0; i < stack.bodies.length; i += 1) {
-    stack.bodies[i].plugin.wrap = {
-        min: { x: render.bounds.min.x, y: render.bounds.min.y },
-        max: { x: render.bounds.max.x, y: render.bounds.max.y }
-    };
-}
-
-*/
